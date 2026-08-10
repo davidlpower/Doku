3f6f64d40e0d3ebadf2ac1e51e7de8c67b5de206
davidlpower <david.lpower@gmail.com>
2026-08-10 12:50:35 +0200

    First attempt at populating the Grid object with values

--- src/doku/grid.py
+++ src/doku/grid.py
@@ -4,4 +4,18 @@ from doku.cell import Cell
 class Grid:
     def __init__(self, puzzle_string):
         w, h = 9, 9
-        self.matrix = [[Cell(puzzle_string[h*w], []) for x in range(w)] for y in range(h)]
+        self.puzzle_string = puzzle_string
+
+        self.matrix = []
+        for row in range(h):
+            temp_row = []
+            for column in range(w):
+                value = self.extract_value_from_puzzle_string(row, column)
+                cell = Cell(value, [])
+                temp_row.append(cell)
+            self.matrix.append(temp_row)
+
+    def extract_value_from_puzzle_string(self, row: int, column: int) -> int:
+        row_starts = [0, 9, 18, 27, 36, 45, 54, 63, 72]
+        index = row_starts[row] + column
+        return int(self.puzzle_string[index])
--- tests/test_grid.py
+++ tests/test_grid.py
@@ -10,10 +10,28 @@ def standard_valid_incomplete() -> str:
 
 @pytest.fixture
 def standard_valid_complete() -> str:
-    """81 Characters — one string of . and int characters"""
+    """81 Characters — one string of int characters"""
     return "316578492529134768487629531263415987974863125851792643138947256692351874745286319"
-
 
 def test_grid_initalisation(standard_valid_complete: str) -> None:
     grid = Grid(standard_valid_complete)
     assert grid.matrix is not None
+
+@pytest.mark.parametrize(
+    ("row", "column", "given_value"),
+    [
+        (0, 0, 3),
+        (1, 1, 2),
+        (2, 2, 7),
+        (3, 3, 4),
+        (4, 4, 6),
+        (5, 5, 2),
+        (6, 6, 2),
+        (7, 7, 7),
+        (8, 8, 9),
+    ],
+)
+def test_extract_given_value_from_puzzle_string(standard_valid_complete: str, row: int, column: int, given_value: int) -> None:
+    grid = Grid(standard_valid_complete)
+    expected_value = grid.extract_value_from_puzzle_string(row, column)
+    assert  expected_value == given_value
